8ddc9a533fc2ae7eebd794be34358a6e79cb0085
Lvl3Mage <76708055+Lvl3Mage@users.noreply.github.com>
2024-12-02 17:55:16 +0100

    DropdownField Added

---
+++ Editor/DropdownFieldDrawer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Lvl3Mage.EditorDevToolkit.Editor;
+using Lvl3Mage.EditorEnhancements.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+namespace Lvl3Mage.EditorEnhancements.Editor
+{
+
+	[CustomPropertyDrawer(typeof(DropdownField), true)]
+	public class DropdownFieldDrawer : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent content)
+		{
+			VerticalLayout layout = GetLayout(property);
+			layout.Draw(position);
+		}
+
+		VerticalLayout GetLayout(SerializedProperty property)
+		{
+			VerticalLayout layout = new();
+			DropdownField attr = attribute as DropdownField;
+			layout.Add(rect => {
+				string[] options = attr.GetOptions(EditorUtils.GetParentObject(property));
+				EditorGUI.BeginChangeCheck();
+				int selected = Array.IndexOf(options,property.stringValue);
+				if(selected == -1){
+					selected = 0;
+				}
+				selected = EditorGUILayout.Popup("My Simple Dropdown", selected, options);
+
+				if (EditorGUI.EndChangeCheck())
+				{
+					property.stringValue = options[selected];
+				}
+
+			}, EditorGUIUtility.singleLineHeight);
+			return layout;
+		}
+
+		public override float GetPropertyHeight(SerializedProperty property, GUIContent content)
+		{
+			VerticalLayout layout = GetLayout(property);
+			return layout.GetHeight();
+		}
+	}
+}
---
+++ Runtime/DropdownField.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Lvl3Mage.EditorEnhancements.Runtime
+{
+	public class DropdownField : PropertyAttribute
+	{
+		EditorDataSource<string[]> optionSource;
+		public DropdownField(string[] options)
+		{
+			optionSource = new EditorDataSource<string[]>(options);
+		}
+		public DropdownField(string sourceName)
+		{
+			optionSource = new EditorDataSource<string[]>(sourceName);
+
+		}
+
+		public string[] GetOptions(object? obj)
+		{
+			if(obj == null){
+				return Array.Empty<string>();
+			}
+			if (optionSource.Get(obj, out string[] result)){
+				return result;
+			}
+			return Array.Empty<string>();
+		}
+	}
+}
